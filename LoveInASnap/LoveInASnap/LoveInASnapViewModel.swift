c0db38fcec8b457ffb7153ed93964dc3062f09e2
Khoa Tran <tlakhoavn@gmail.com>
2021-11-30 23:53:27 +0700

    Improving Image Quality

--- LoveInASnap/LoveInASnap/LoveInASnapViewModel.swift
+++ LoveInASnap/LoveInASnap/LoveInASnapViewModel.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SwiftUI
 import TesseractOCR
-//import GPUImage
+import GPUImage
 
 class LoveInASnapViewModel: ObservableObject {
     @Published private(set) var recognizedText = "Nothing here..."
@@ -19,9 +19,38 @@ class LoveInASnapViewModel: ObservableObject {
         if let tesseract = G8Tesseract(language: "eng+fra") {
             tesseract.engineMode = .tesseractCubeCombined
             tesseract.pageSegmentationMode = .auto
-            tesseract.image = image
+            let scaledImage = image.scaledImage(1000) ?? image
+            let preprocessedImage = scaledImage.preprocessedImage() ?? scaledImage
+            tesseract.image = preprocessedImage
             tesseract.recognize()
             recognizedText = tesseract.recognizedText ?? String()
         }
+    }
+}
+
+// MARK: - UIImage extension
+
+extension UIImage {
+    func scaledImage(_ maxDimension: CGFloat) -> UIImage? {
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        
+        if size.width > size.height {
+            scaledSize.height = size.height / size.width * scaledSize.width
+        } else {
+            scaledSize.width = size.width / size.height * scaledSize.height
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return scaledImage
+    }
+    
+    func preprocessedImage() -> UIImage? {
+        let stillImageFilter = GPUImageAdaptiveThresholdFilter()
+        stillImageFilter.blurRadiusInPixels = 15.0
+        let filteredImage = stillImageFilter.image(byFilteringImage: self)
+        return filteredImage
     }
 }
